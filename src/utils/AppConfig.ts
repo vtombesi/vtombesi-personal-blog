887f4b714d290769f582672071a7e9b236588920
Valentino Tombesi <valentino.tombesi@gmail.com>
2023-10-30 19:23:33 +0100

    updated

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,7 +1,7 @@
 export const AppConfig = {
   site_name: "Valentino's blog",
   title: "Valentino's blog",
-  description: 'Boilerplate built with Astro using React and Tailwind CSS',
+  description: 'A personal blog for a happy developer',
   author: 'Valentino',
   locale_region: 'en-us',
   locale: 'en',
